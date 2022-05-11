2d7e4df1ba221c207347721e29d729ac2fcfdae3
aamenkov <aamenkov@mail.ru>
2022-05-11 16:43:30 +0300

    Add new controllers

---
+++ DeliveryCompanyWebApi/Controllers/ApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DeliveryCompanyData.Entities;
+using DeliveryCompanyDataAccessEF.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryCompanyWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ApplicationController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IEnumerable<Application> GetAllPersons()
+        {
+            return _unitOfWork.Application.GetAll();
+        }
+    }
+}
---
+++ DeliveryCompanyWebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using DeliveryCompanyDataAccessEF.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryCompanyWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DepartmentController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
