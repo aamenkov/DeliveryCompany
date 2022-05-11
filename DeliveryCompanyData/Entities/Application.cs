c12651c08ef574ed88e17222d08d16b1393bc36e
aamenkov <aamenkov@mail.ru>
2022-05-11 16:41:26 +0300

    Add entities

---
+++ DeliveryCompanyData/Entities/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryCompanyData.Entities
+{
+    public class Application
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ApplicationId { get; set; }
+        public string ReceivingAddress { get; set; }
+        public string ReceivingTown { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryTown { get; set; }
+        public int Weight { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool IsWorking { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        [ForeignKey("DepartmentInfoKey")]
+        public Guid DepartmentId { get; set; }
+    }
+}
---
+++ DeliveryCompanyData/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Transactions;
+
+namespace DeliveryCompanyData.Entities
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid DepartmentId { get; set; }
+        public List<Application> ApplicationList { get; set; }
+    }
+}
